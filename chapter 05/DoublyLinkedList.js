de9a5b7e36ec77e6fad59b5f3237e53666142236
Wendell Alves <wendellmccaulay@hotmail.com>
2023-04-28 21:49:55 -0300

    Doubly Linked List

---
+++ chapter 05/DoublyLinkedList.js
@@ -0,0 +1,97 @@
+class DoublyNode {
+    
+    constructor(element) {
+        this.element = element;
+        this.next = null;
+        this.prev = null;
+    }
+
+}
+
+class DoublyLinkedList {
+
+    constructor() {
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    }
+
+    insert(position, element) {
+        if (position >= 0 && position <= this.length) {
+            let node = new DoublyNode(element);
+            let current = this.head;
+            let previous = null;
+            let index = 0;
+
+            if (position === 0) {
+                if (!this.head) {
+                    this.head = node;
+                    this.tail = node;
+                } else {
+                    node.next = current;
+                    current.prev = node;
+                    this.head = node;
+                }
+            } else if (position === this.length) {
+                current = this.tail;
+                current.next = node;
+                node.prev = current;
+                this.tail = node;
+            } else {
+                while (index++ < position) {
+                    previous = current;
+                    current = current.next;
+                }
+
+                node.next = current;
+                previous.next = node;
+
+                current.prev = node;
+                node.prev = previous;
+            }
+
+            this.length++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    removeAt(position) {
+        if (position > -1 && position < this.length) {
+            let current = this.head;
+            let previous = null;
+            let index = 0;
+
+            if (position === 0) {
+                this.head = current.next;
+
+                if (this.length === 1) {
+                    this.tail = null;
+                } else {
+                    this.head.prev = null;
+                }
+            } else if (position === this.length) {
+                current = this.tail;
+                this.tail = current.prev;
+                this.tail.next = null;
+            } else {
+                while (index++ < position) {
+                    previous = current;
+                    current = current.next;
+                }
+
+                previous.next = current.next;
+                current.next.prev = previous;
+            }
+            this.length--;
+            return current.element;
+        } else {
+            return null;
+        }
+    }
+
+}
+
+module.exports = DoublyLinkedList;
+
